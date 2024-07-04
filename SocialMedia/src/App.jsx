8b48f53a7dda9ded240ab413ab35e5e804b111cb
Asthaviradiya <asthaviradiya114@gmail.com>
2024-07-04 19:46:08 +0530

    component files are updated

--- SocialMedia/src/App.jsx
+++ SocialMedia/src/App.jsx
@@ -4,18 +4,22 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import SideBar from './components/SideBar'
 import CreatePost from './components/CreatePost'
-import Post from './components/Post'
+import PostList from './components/PostList'
+import { useState } from 'react'
 
 function App() {
 
+
+  const[selectedTab, setSelectedTab] = useState("Home");
   return (
     
     <div className='app-container'>
-    <SideBar></SideBar>
+    <SideBar selectedTab = {selectedTab} setSelectedTab = {setSelectedTab}></SideBar>
       <div className='content'>
       <Header></Header>
-      <CreatePost></CreatePost>
-      <Post></Post>
+      {selectedTab === 'Home' ? <PostList></PostList> : <CreatePost></CreatePost>}
+      
+      
       <Footer></Footer>
       </div>
       </div>
