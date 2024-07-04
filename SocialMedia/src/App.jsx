e76af2e99fcf7c21dffbc5cd5a584a32c91a8d30
Asthaviradiya <asthaviradiya114@gmail.com>
2024-07-04 19:04:23 +0530

    postlist jsx file added

--- SocialMedia/src/App.jsx
+++ SocialMedia/src/App.jsx
@@ -4,6 +4,7 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import SideBar from './components/SideBar'
 import CreatePost from './components/CreatePost'
+import Post from './components/Post'
 
 function App() {
 
@@ -14,6 +15,7 @@ function App() {
       <div className='content'>
       <Header></Header>
       <CreatePost></CreatePost>
+      <Post></Post>
       <Footer></Footer>
       </div>
       </div>
