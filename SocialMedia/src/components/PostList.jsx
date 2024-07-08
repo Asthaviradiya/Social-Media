8be4bf983f1aaea7ff1d2639c74a59e7c2b99e94
Asthaviradiya <asthaviradiya114@gmail.com>
2024-07-08 10:32:00 +0530

    PostList jsx : postlist updated

--- SocialMedia/src/components/PostList.jsx
+++ SocialMedia/src/components/PostList.jsx
@@ -1,10 +1,12 @@
+import { useContext } from "react";
 import Post from "./Post";
+import {PostList as PostListData} from "../store/post-list-store";
 const PostList = () => {
-    return <>
-    <Post></Post>
-    <Post></Post>
-    <Post></Post>
-    <Post></Post>
+    const {postLists} = useContext(PostListData);
+    return(
+         <>
+    {postLists.map((post) => (<Post/>))}
     </>
-}
+    );
+};
 export default PostList;
